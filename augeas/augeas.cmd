35b8eb3df484743643517d4df5c8b06c484f5b57
Marcel <marcelstanley@users.noreply.github.com>
2015-11-25 16:38:58 -0200

    Update logging subsytem version to 3.0.

--- augeas/augeas.cmd
+++ augeas/augeas.cmd
@@ -1,7 +1,7 @@
 set /augeas/load/Xml/lens Xml.lns
 set /augeas/load/Xml/incl[2] /opt/wildfly/standalone/configuration/standalone.xml
 load
-defvar subsystem "/files/opt/wildfly/standalone/configuration/standalone.xml/server/profile/subsystem[#attribute/xmlns='urn:jboss:domain:logging:2.0']"
+defvar subsystem "/files/opt/wildfly/standalone/configuration/standalone.xml/server/profile/subsystem[#attribute/xmlns='urn:jboss:domain:logging:3.0']"
 set $subsystem/console-handler/level/#attribute/name "DEBUG"
 set $subsystem/root-logger/level/#attribute/name "DEBUG"
 set $subsystem/logger[last()+1]/#attribute/category "pl.goldmann.example"
